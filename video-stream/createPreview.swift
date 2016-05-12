428c8156feec5fb5d8d621cd934eb344e01e65ef
RomainRui <romain.rui10@gmail.com>
2016-05-12 17:19:20 +0200

    Create Session

---
+++ video-stream/createPreview.swift
@@ -0,0 +1,16 @@
+//
+//  createPreview.swift
+//  video-stream
+//
+//  Created by Rplay on 12/05/16.
+//  Copyright © 2016 rplay. All rights reserved.
+//
+
+import Foundation
+
+class preview {
+    
+    func createPreview() ->  {
+        
+    }
+}
